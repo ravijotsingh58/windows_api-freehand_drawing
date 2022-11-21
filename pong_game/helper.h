b0f0f7353b3348ff6285a577b64fdf4a08c66ab3
Ravijot singh <Ravijotsingh427@gmail.com>
2022-11-21 17:04:49 +0530

    Create helper.h

---
+++ pong_game/helper.h
@@ -0,0 +1,61 @@
+#ifndef __HELPER_H
+#define __HELPER_H
+
+#include<stdio.h>
+#include<time.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE hInst; //Current Instance
+COLORREF Grey = RGB(40, 40, 40);
+
+/*
+FUNCTION: InitInstance(HANDLE, int, char*)
+PURPOSE: saves instance handle and creates main window
+COMMENTS: In this function, we saves the Instance handle in a global variable
+and creates and displays main window
+*/
+
+BOOL InitInstance(HINSTANCE hInstance, int nCmdShow, char* pTitle){
+    
+    char className[] = "MyWindowClass";
+    HWND hWnd;
+    WNDCLASSEX wcex;
+    wcex.cbSize = sizeof(WNDCLASSEX);
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = (WNDPROC) WndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = NULL;
+    wcex.hCursor = LoadCursor(NULL, IDC_ARROW);;
+    wcex.hbrBackground = (HBRUSH)CreateSolidBrush(Grey);
+    wcex.lpszMenuName = NULL;
+    wcex.lpszClassName = className;
+    wcex.hIconSm = NULL;
+
+    if(!RegisterClassEx(&wcex)){
+        return FALSE;
+    }
+
+    hInst = hInstance;
+
+    hWnd = CreateWindowA(className, pTitle, WS_OVERLAPPEDWINDOW & ~WS_MAXIMIZEBOX & ~WS_THICKFRAME, CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, NULL, NULL, hInstance, NULL);
+
+    if(!hWnd){
+        return FALSE;
+    }
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    return TRUE; 
+}
+
+double generateRandominRange(int lower, int upper) {
+    
+    srand(time(0));
+
+    return (rand() %
+        (upper - lower + 1)) + lower;
+}
+#endif
